1435f6398531e94cd6d3681dfb3bb7e1fd9047dc
pablojose.garcia.lopez.contractor <pablojose.garcia.lopez.contractor@bbva.com>
2022-03-19 17:23:36 +0100

    Add rest of config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,38 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'airbnb'
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
-    'react',
+    'react'
   ],
   rules: {
-  },
+    'class-methods-use-this': 0,
+    'comma-dangle': [2, 'never'],
+    'no-underscore-dangle': 0,
+    'no-shadow': 0,
+    'object-curly-newline': 0,
+    'react/prop-types': 0,
+    'linebreak-style': 0,
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 0,
+    'react/react-in-jsx-scope': 'off'
+
+  }
 };
